647ea7dc614e0425528edd94f0742cbd8dacf542
aldls <jealswl04@gmail.com>
2025-01-07 13:25:49 +0900

    다시 돌아가기 고치기 전전

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -61,9 +61,6 @@ export class State extends Schema {
     @type("boolean")
     gameOver: boolean = false;
 
-    @type("number")
-    selectButton: number = 0;
-
     something = "This attribute won't be sent to the client-side";
 
     // 메서드
